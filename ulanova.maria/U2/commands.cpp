02d26322b69782b6f508a1829139355a32807bbc
vamaria06-ux <ulanovamaria0906@yandex.ru>
2026-06-25 12:32:02 +0300

    Add meets, less, greater commands

--- ulanova.maria/U2/commands.cpp
+++ ulanova.maria/U2/commands.cpp
@@ -125,6 +125,84 @@ namespace ulanova
     }
   }
 
+  static void printMeetsSorted(const Array< Meet > & meets,
+      size_t id,
+      std::ostream & out)
+  {
+    Array< Meet > tmp = mArray< Meet >();
+    for (size_t i = 0; i < meets.size; ++i)
+    {
+      if (meets.data[i].id1 == id || meets.data[i].id2 == id)
+      {
+        pushBack(tmp, meets.data[i]);
+      }
+    }
+    for (size_t i = 0; i < tmp.size; ++i)
+    {
+      for (size_t j = i + 1; j < tmp.size; ++j)
+      {
+        const size_t othI = tmp.data[i].id1 == id ? tmp.data[i].id2 : tmp.data[i].id1;
+        const size_t othJ = tmp.data[j].id1 == id ? tmp.data[j].id2 : tmp.data[j].id1;
+        if (othI > othJ || (othI == othJ && tmp.data[i].duration > tmp.data[j].duration))
+        {
+          const Meet m = tmp.data[i];
+          tmp.data[i] = tmp.data[j];
+          tmp.data[j] = m;
+        }
+      }
+    }
+    for (size_t i = 0; i < tmp.size; ++i)
+    {
+      const size_t other = tmp.data[i].id1 == id ? tmp.data[i].id2 : tmp.data[i].id1;
+      out << other << " " << tmp.data[i].duration << "\n";
+    }
+    clear(tmp);
+  }
+
+  static void cmdLessGreater(const Array< Meet > & meets,
+      size_t id,
+      size_t time,
+      bool less,
+      std::ostream & out)
+  {
+    Array< Meet > tmp = mArray< Meet >();
+    for (size_t i = 0; i < meets.size; ++i)
+    {
+      if (meets.data[i].id1 != id && meets.data[i].id2 != id)
+      {
+        continue;
+      }
+      if (less && meets.data[i].duration < time)
+      {
+        pushBack(tmp, meets.data[i]);
+      }
+      else if (!less && meets.data[i].duration > time)
+      {
+        pushBack(tmp, meets.data[i]);
+      }
+    }
+    for (size_t i = 0; i < tmp.size; ++i)
+    {
+      for (size_t j = i + 1; j < tmp.size; ++j)
+      {
+        const size_t othI = tmp.data[i].id1 == id ? tmp.data[i].id2 : tmp.data[i].id1;
+        const size_t othJ = tmp.data[j].id1 == id ? tmp.data[j].id2 : tmp.data[j].id1;
+        if (othI > othJ || (othI == othJ && tmp.data[i].duration > tmp.data[j].duration))
+        {
+          const Meet m = tmp.data[i];
+          tmp.data[i] = tmp.data[j];
+          tmp.data[j] = m;
+        }
+      }
+    }
+    for (size_t i = 0; i < tmp.size; ++i)
+    {
+      const size_t other = tmp.data[i].id1 == id ? tmp.data[i].id2 : tmp.data[i].id1;
+      out << other << " " << tmp.data[i].duration << "\n";
+    }
+    clear(tmp);
+  }
+
   void runCommands(Array< Person > & persons,
       Array< Meet > & meets,
       std::istream & in,
@@ -176,6 +254,38 @@ namespace ulanova
           desc += ch;
         }
         cmdRedesc(persons, id, desc);
+      }
+      else if (cmd == "meets")
+      {
+        size_t id = 0;
+        if (!(in >> id))
+        {
+          out << "<INVALID COMMAND>\n";
+          continue;
+        }
+        printMeetsSorted(meets, id, out);
+      }
+      else if (cmd == "less")
+      {
+        size_t time = 0;
+        size_t id = 0;
+        if (!(in >> time >> id))
+        {
+          out << "<INVALID COMMAND>\n";
+          continue;
+        }
+        cmdLessGreater(meets, id, time, true, out);
+      }
+      else if (cmd == "greater")
+      {
+        size_t time = 0;
+        size_t id = 0;
+        if (!(in >> time >> id))
+        {
+          out << "<INVALID COMMAND>\n";
+          continue;
+        }
+        cmdLessGreater(meets, id, time, false, out);
       }
       else
       {
