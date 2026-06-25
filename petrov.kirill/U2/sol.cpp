dbbfbc751f84a1c262a1d9be65b37d73b555a559
Kirill <kirillpetrov56333@gmail.com>
2026-06-25 12:46:37 +0300

    adding begin of cycle in sol

--- petrov.kirill/U2/sol.cpp
+++ petrov.kirill/U2/sol.cpp
@@ -17,4 +17,33 @@ namespace petrov
     data = new_d;
     c = new_c;
   }
+
+  size_t cycle(int argc, char** argv)
+  {
+    if (argc < 2 || argc > 3)
+    {
+      std::cerr << "too much\n";
+      return 1;
+    }
+    std::string inf = "", df = "";
+    for (int i = 1; i < argc; ++i) {
+      std::string arg = argv[i];
+      if (!arg.rfind("in:", 0)) {
+        if (!inf.empty()) {
+          std::cerr << "dubl\n";
+          return 1;
+        }
+        inf = arg.substr(3);
+      } else if (!arg.rfind("data:", 0)) {
+        if (!df.empty()) {
+          std::cerr << "dubl\n";
+          return 1;
+        }
+        df = arg.substr(5);
+      } else {
+        std::cerr << "inval\n";
+        return 1;
+      }
+    }
+  }
 }
